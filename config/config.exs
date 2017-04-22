cd2c1d8faf4baa40ab67f23e3977399dcc527f97
Jacek Kubiak <jk.kubiak@gmail.com>
2017-04-22 13:33:18 +0200

    Update config file and router

--- config/config.exs
+++ config/config.exs
@@ -25,3 +25,18 @@ config :logger, :console,
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
+
+# %% Coherence Configuration %%   Don't remove this line
+config :coherence,
+  user_schema: TinderClone.User,
+  repo: TinderClone.Repo,
+  module: TinderClone,
+  logged_out_url: "/",
+  email_from_name: "TinderClone",
+  email_from_email: "tinder_clone@example.com",
+  opts: [:authenticatable, :recoverable, :lockable, :trackable, :unlockable_with_token, :invitable, :registerable]
+
+config :coherence, TinderClone.Coherence.Mailer,
+  adapter: Swoosh.Adapters.Sendgrid,
+  api_key: "your api key here"
+# %% End Coherence Configuration %%
