d7b007f6c86ac9302e94eddc7ec6b54112f2bd01
Jacek Kubiak <jk.kubiak@gmail.com>
2017-04-25 10:56:14 +0200

    Add view helpers

--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -1,3 +1,22 @@
 defmodule TinderClone.PageView do
   use TinderClone.Web, :view
+  alias TinderClone.{User, Match}
+
+  def contact_link(conn, user_id) do
+    cur_user_id = conn.assigns.current_user.id
+    if User.favorited_by?(cur_user_id, user_id) do
+      link("Remove from favorites", to: "/contact/#{user_id}", method: :delete)
+    else
+      link("Add to favorites", to: "/contact/#{user_id}", method: :post)
+    end
+  end
+
+  def chat_link(conn, user_id) do
+    cur_user_id = conn.assigns.current_user.id
+    if User.matched?(cur_user_id, user_id) do
+      room_name = Match.room_name_for(cur_user_id, user_id)
+      link("Chat with me!", to: "/private_room/#{room_name}")
+
+    end
+  end
 end
