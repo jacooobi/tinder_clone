72babc30ed35dedd5f045f3810233728a1210f52
Jacek Kubiak <jk.kubiak@gmail.com>
2017-04-25 10:50:35 +0200

    Add relationship to user and a couple of helper functions

--- web/models/coherence/user.ex
+++ web/models/coherence/user.ex
@@ -2,9 +2,12 @@ defmodule TinderClone.User do
   use TinderClone.Web, :model
   use Coherence.Schema
 
+  alias TinderClone.{Repo, User}
+
   schema "users" do
     field :name, :string
     field :email, :string
+    many_to_many :contacts, TinderClone.User, join_through: TinderClone.Contact, join_keys: [user_id: :id, contact_id: :id]
     coherence_schema
 
     timestamps
@@ -18,4 +21,16 @@ defmodule TinderClone.User do
     |> unique_constraint(:email)
     |> validate_coherence(params)
   end
-end
+
+  def favorited_by?(user_favoriting_id, user_favorited_id) do
+    Repo.get(User, user_favoriting_id)
+    |> Repo.preload(:contacts)
+    |> Map.fetch!(:contacts)
+    |> Enum.any?(fn(c) -> c.id == user_favorited_id end)
+  end
+
+  def matched?(user_a, user_b) do
+    favorited_by?(user_a, user_b) && favorited_by?(user_b, user_a)
+  end
+ end
+
