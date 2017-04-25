376b52f21d5ba12d5680a11a7ff7ec4df00e1960
Jacek Kubiak <jk.kubiak@gmail.com>
2017-04-25 12:02:12 +0200

    Fix failing test

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule TinderClone.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Please log in before entering the chat"
   end
 end
