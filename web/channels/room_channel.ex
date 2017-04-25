a17bb5df7c2f2670e206633fc1dec820b4b5c622
Jacek Kubiak <jk.kubiak@gmail.com>
2017-04-25 11:02:01 +0200

    Update RoomChannel to support private rooms and perform authentication on joining

--- web/channels/room_channel.ex
+++ web/channels/room_channel.ex
@@ -1,11 +1,35 @@
 defmodule TinderClone.RoomChannel do
   use Phoenix.Channel
 
-  def join("room:lobby", _message, socket) do
-    {:ok, socket}
+  import Ecto.Query
+  alias TinderClone.{Match, Repo, User}
+
+  def join("room:lobby", params, socket) do
+    case Phoenix.Token.verify(socket, "user_id", params["token"], max_age: 1000000) do
+      {:ok, user_id} -> {:ok, assign(socket, :user_id, user_id)}
+      {:error, _reason} -> {:error, %{reason: "unauthorized"}}
+    end
   end
 
-  def join("room:" <> private_room_id, _params, _socket) do
-    {:error, %{reason: "unauthorized"}}
+  def join("room:" <> private_room_id, params, socket) do
+    q = from m in Match, where: m.room_name == ^private_room_id, limit: 1
+    [room] = Repo.all(q)
+
+
+     case Phoenix.Token.verify(socket, "user_id", params["token"], max_age: 1000000) do
+      {:ok, user_id} ->
+        if Enum.any?([room.user_a_id, room.user_b_id], fn(id) -> id == user_id end) do
+          {:ok, assign(socket, :user_id, user_id)}
+        end
+      {:error, _reason} ->
+        {:error, %{reason: "unauthorized"}}
+      end
+  end
+
+  def handle_in("new_msg", payload, socket) do
+    user = Repo.get(User, socket.assigns.user_id)
+
+    broadcast! socket, "new_msg", %{user: user.name, body: payload["body"]}
+    {:noreply, socket}
   end
 end
