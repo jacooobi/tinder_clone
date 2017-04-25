a7e07d66f8566bf4dcb8442acea8288f61c7e776
Jacek Kubiak <jk.kubiak@gmail.com>
2017-04-25 10:55:22 +0200

    Fetch all registered users without current one in the main chat lobby

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,7 +1,16 @@
 defmodule TinderClone.PageController do
   use TinderClone.Web, :controller
 
+  alias TinderClone.{Repo, User}
+
   def index(conn, _params) do
     render conn, "index.html"
+    users = conn.assigns[:current_user].id |> users_list_without_current
+    render conn, "index.html", users: users
+  end
+
+  defp users_list_without_current(user_id) do
+    query = from u in "users", where: not u.id in [^user_id], select: %{id: u.id, email: u.email, name: u.name}
+    query |> Repo.all()
   end
 end
