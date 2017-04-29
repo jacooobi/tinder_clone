8f6775c874a9b1cebd369f001a2ed161fbfee698
Jacek Kubiak <jk.kubiak@gmail.com>
2017-04-29 10:15:18 +0200

    Update RoomChannel

--- web/channels/room_channel.ex
+++ web/channels/room_channel.ex
@@ -1,22 +1,19 @@
 defmodule TinderClone.RoomChannel do
   use Phoenix.Channel
 
-  import Ecto.Query
   alias TinderClone.{Match, Repo, User}
 
   def join("room:lobby", params, socket) do
-    case Phoenix.Token.verify(socket, "user_id", params["token"], max_age: 1000000) do
+    case user_verification(socket, params["token"]) do
       {:ok, user_id} -> {:ok, assign(socket, :user_id, user_id)}
       {:error, _reason} -> {:error, %{reason: "unauthorized"}}
     end
   end
 
   def join("room:" <> private_room_id, params, socket) do
-    q = from m in Match, where: m.room_name == ^private_room_id, limit: 1
-    [room] = Repo.all(q)
+    room = Repo.get_by(Match, room_name: private_room_id)
 
-
-     case Phoenix.Token.verify(socket, "user_id", params["token"], max_age: 1000000) do
+     case user_verification(socket, params["token"]) do
       {:ok, user_id} ->
         if Enum.any?([room.user_a_id, room.user_b_id], fn(id) -> id == user_id end) do
           {:ok, assign(socket, :user_id, user_id)}
@@ -31,5 +28,9 @@ defmodule TinderClone.RoomChannel do
 
     broadcast! socket, "new_msg", %{user: user.name, body: payload["body"]}
     {:noreply, socket}
+  end
+
+  defp user_verification(socket, token) do
+    Phoenix.Token.verify(socket, "user_id", token, max_age: 1000000)
   end
 end
