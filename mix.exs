a0250e62f03704cb58571f9123f3b1f62273fd89
Jacek Kubiak <jk.kubiak@gmail.com>
2017-04-29 10:08:47 +0200

    Add ex_machina package

--- mix.exs
+++ mix.exs
@@ -38,7 +38,8 @@ defmodule TinderClone.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
-     {:coherence, "~> 0.3"}]
+     {:coherence, "~> 0.3"},
+     {:ex_machina, "~> 2.0", only: :test}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
